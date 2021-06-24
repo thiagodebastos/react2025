ae6245f516a61e00745cc2c02f29c6d9475abce3
Thiago de Bastos <thiago@lilacstudio.com.au>
2021-06-24 23:10:40 +0000

    update headline (#2)

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Fast Feedback</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Fast Feedback</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
