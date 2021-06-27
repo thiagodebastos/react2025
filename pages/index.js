a89feb0d5af6452375d71b563338273fd3ddae3e
Thiago de Latierra <thiago@lilacstudio.com.au>
2021-06-27 13:43:47 +1000

    try to force push...

--- pages/index.js
+++ pages/index.js
@@ -8,47 +8,25 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Fast Feedback</title>
+        <title>Fast Feedback - React 2025!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Fast Feedback</h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {auth.loading && <div>Loading...</div>}
+        {auth.user && (
+          <>
+            <div>{auth.user.name}</div>
+            <div>{auth.user.email}</div>
+            <div>{auth.user.provider}</div>
+            <button onClick={(_e) => auth.signout()}>Sign Out</button>
+          </>
+        )}
+        {!auth.user && !auth.loading && (
+          <button onClick={(_e) => auth.signInWithGithub()}>
+            Sign In with Github
+          </button>
+        )}
       </main>
 
       <footer className={styles.footer}>
