d9108f19cb2e918a61aff8c5c5b104ce6fd4b82e
Thiago de Latierra <thiago@lilacstudio.com.au>
2021-06-27 18:42:09 +1000

    add chakra-ui with default theme

---
+++ styles/theme.js
@@ -0,0 +1,12 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  fonts: {
+    body: 'Rubik, sans-serif',
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 600,
+    bold: 800,
+  },
+})
